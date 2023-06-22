5f3274367763a08ebcd8a0024353ab2b37949435
Yaroslav Borys <borrline@gmail.com>
2023-06-22 13:26:06 +0300

    attempt

--- controllers/repair/updateStatusById.js
+++ controllers/repair/updateStatusById.js
@@ -16,10 +16,16 @@ const updateStatusById = async (req, res, next) => {
     if (!result) {
       throw createError(404, `Phone with id=${id} not found`);
     }
+
+    const allRepair = await Repair.find({});
+    const diagnosis = allRepair.filter((item) => item.status === "diagnosis");
+    const repair = allRepair.filter((item) => item.status === "repair");
+    const purchase = allRepair.filter((item) => item.status === "purchase");
+
     res.json({
       status: "success",
       code: 200,
-      result,
+      allRepair,
     });
   } catch (error) {
     next(new ErrorHandler(error.statusCode || 500, error.message));
